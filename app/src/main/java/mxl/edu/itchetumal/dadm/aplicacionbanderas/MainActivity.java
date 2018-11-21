c3d5130967ede65fb0cf95e0f0b056cca0a65359
NayraHernandez <15390229@itchetumal.edu.mx>
2018-11-21 13:10:26 -0500

    Muestra nombre del pais, bandera y dominio. Uso de los arrays (dominiospaises y paises)

--- app/src/main/java/mxl/edu/itchetumal/dadm/aplicacionbanderas/MainActivity.java
+++ app/src/main/java/mxl/edu/itchetumal/dadm/aplicacionbanderas/MainActivity.java
@@ -1,82 +1,31 @@
 package mxl.edu.itchetumal.dadm.aplicacionbanderas;
-import android.content.Context;
-import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
-import android.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import java.util.List;
-import java.util.zip.Inflater;
+import android.content.res.Resources;
 
 public class MainActivity extends AppCompatActivity {
-    private Pais[] datos;
+
+    String[] Paises;
+    String[] Dominios;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<CharSequence> adaptadorPaises = ArrayAdapter.createFromResource(this,
-                R.array.array_paises,
-                android.R.layout.simple_spinner_item);
-
-        Spinner spPaises = (Spinner) findViewById(R.id.cmbPaises);
-
-        adaptadorPaises.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item);
-
-        spPaises.setAdapter(adaptadorPaises);
-
-        ArrayAdapter<CharSequence> adaptadorListaPaises =
-                ArrayAdapter.createFromResource(this,
-                        R.array.array_dominiopaises,
-                        android.R.layout.simple_list_item_checked);
+        Resources res = getResources();
+        //Llamar los recursos de los arreglos
+        Paises = res.getStringArray(R.array.array_paises);
+        Dominios = res.getStringArray(R.array.array_dominiopaises);
 
         ListView lstNombresPaises = (ListView) findViewById(R.id.lstPaises);
-
-        lstNombresPaises.setAdapter(adaptadorListaPaises);
-        spPaises.setAdapter(adaptadorPaises);
-
-        ArrayAdapter<CharSequence> adaptadorListaPaises = android.R.layout.simple_list_item_1;
-
-       // Pais miPais= new Pais (R.drawable.mexico, "México",".mx");
-        datos = new Pais[] {new Pais (R.drawable.mexico, "México",".mx"),
-                new Pais (R.drawable.francia, "Francia",".fr")
-                new Pais (R.drawable.alemania, "Alemania",".de")};
+        Pais myAdapter = new Pais(getApplicationContext(), Paises, Dominios);
+        lstNombresPaises.setAdapter(myAdapter);
 
 
-                AdaptadorPaises adaptador = new AdaptadorPaises(this);
-                ListView lstNombresPaises = (ListView) findViewById(R.id.lstPaises);
-                lstNombresPaises.setAdapter(adaptadorListaPaises);
-                lstNombresPaises.setAdapter(adaptador);
-                lstNombresPaises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        // Get the selected item text from ListView
-                        String selectedItem = (String) parent.getItemAtPosition(position);
-                        // Display the selected item text on TextView
-                        Toast.makeText(getBaseContext(),"Your favorite : " + selectedItem, Toast.LENGTH_LONG).show();
-                    }
-        });
     }
-
-    class AdaptadorPaises extends ArrayAdapter <Pais>{
-
-        public AdaptadorPaises (Context context){
-            super (context, R.layout.lista_paises, datos);
-        }
-        public View getView(int position, View convertView, ViewGroup parent){
-            LayoutInflater = inflater = LayoutInflater.from(getContext());
-            View item = inflater.inflate (R.layout.lista_paises, null);
-        }
-    }
-
 }
+
 
--- app/src/main/java/mxl/edu/itchetumal/dadm/aplicacionbanderas/Pais.java
+++ app/src/main/java/mxl/edu/itchetumal/dadm/aplicacionbanderas/Pais.java
@@ -1,22 +1,70 @@
 package mxl.edu.itchetumal.dadm.aplicacionbanderas;
 
-public class Pais {
-    private int bandera;
-    private String nombre;
-    private  String dominio;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-    public  Pais(int bandera, String nombre, String dominio){
-        this.bandera = bandera;
-        this.nombre = nombre;
-        this.dominio = dominio;
+public class Pais extends BaseAdapter{
+
+    private Integer[] Banderas = {
+            R.drawable.francia,
+            R.drawable.inglaterra,
+            R.drawable.venezuela,
+            R.drawable.mexico,
+            R.drawable.suecia,
+            R.drawable.alemania,
+            R.drawable.usa,
+            R.drawable.polonia,
+            R.drawable.coreasur,
+            R.drawable.italia,
+    };
+
+    private String [] Paises;
+    private String [] Dominio;
+    private Context context;
+    private LayoutInflater thisInflater;
+
+    public Pais(Context con, String[] Nombre, String[] Dominio) {
+        this.context = con;
+        this.thisInflater = LayoutInflater.from(con);
+        this.Paises = Nombre;
+        this.Dominio = Dominio;
     }
-    public int getBandera(){
-        return  bandera;
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if (convertView == null){
+            convertView = thisInflater.inflate(R.layout.lista_paises, parent, false);
+
+            TextView lblNombre = (TextView)convertView.findViewById(R.id.lblNombre);
+            TextView lblDominio = (TextView)convertView.findViewById(R.id.lblDominio);
+            ImageView imgBand = (ImageView) convertView.findViewById(R.id.imgBandera);
+
+            lblNombre.setText(Paises[position]);
+            lblDominio.setText(Dominio[position]);
+            imgBand.setImageResource(Banderas[position]);
+        }
+        return convertView;
     }
-    public String getNombre(){
-        return nombre;
+    @Override
+    public int getCount() {
+
+        return Banderas.length;
     }
-    public String getDominio(){
-        return dominio;
+
+    @Override
+    public Object getItem(int position) {
+
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+
+        return position;
     }
 }
